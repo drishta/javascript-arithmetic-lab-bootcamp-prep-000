799523bd241bd202a8c8f3e93a7b283ea1725757
Austin Katzer <austin484@gmail.com>
2018-05-16 21:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 function add(a, b) 
 {
-  return parseInt(a) + parseInt(b)
+  return a + b
 }
 
-function substract(a, b)
+function subtract(a, b)
 {
-  return parseInt(a) - parseInt(b)
+  return a - b
 }
 
 function multiply(a, b)
 {
-  return parseInt(a) * parseInt(b)
+  return a * b
 }
 
 function divide(a, b)
 {
   return a / b
+}
+
+function inc(n)
+{
+  return n += 1 
+}
+
+function dec(n)
+{
+  return n -= 1
+}
+
+function makeInt(n)
+{
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n)
+{
+  return parseFloat(n)
 }
